2e580c79c2aa7c2d5b05f754db8585d18d3c61bb
nywxy <150626630@qq.com>
2019-11-28 21:28:24 +0800

    init UI

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-from dataoption import dbase
+# from dataoption import dbase
 
-db = dbase()
+# db = dbase()
 
-db.dbOptionWithSql("select * from TermTable where term>2019000")
-print(db.cursor.fetchall())
+# db.dbOptionWithSql("select * from TermTable where term>2019000")
+# print(db.cursor.fetchall())
+
+from ui.jxluckymain import *
+import sys
+from PyQt5 import QtCore,QtGui,QtWidgets
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
