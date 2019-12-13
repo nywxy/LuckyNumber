a3283d027d71a8c57b646b333a7ec0bb20c00718
nywxy <150626630@qq.com>
2019-12-13 18:27:42 +0800

    add call roll

--- calmodule.py
+++ calmodule.py
@@ -83,6 +83,30 @@ class calModule():
         end = time.clock()
         print("生成概率周期数据完毕，用时：",end-start)
 
+    def initDataWithZone(self,page=33,scope=100,zone=32):
+        self.__page = page
+        self.__scope = scope
+        # #重新生成表的话，就把原来的表全部清空，重头再来一次
+        # if self.__modb.isOnline:
+        #     self.__modb.clearTermTable()
+        # else:
+        #     print("离线状态下将使用原有数据进行分析！")
+        # # 首先生成TermTable中的数据
+        # self.__modb.createTermTable()
+        # #不管在线或者离线状态下均可执行数据及概率表的重新生成
+        # self.__modb.clearLuckyTable()
+        # self.__modb.clearDataInterval()
+        #生成LuckyTable表
+        #通过多线程生成
+        self.createAllLuckyDataWithZone(self.__scope,self.__page,zone)
+
+        # print("开始生成概率周期数据")
+        # start = time.clock()
+        # for data in self.__modb.getAllDataInterval():
+        #     self.__modb.updateInterval(data)
+        # end = time.clock()
+        # print("生成概率周期数据完毕，用时：",end-start)
+
     #此方法为已有最新数据后的生成预测，只有再初始化数据或更新数据后可用
     def createForecastData(self):
         #生成一个termdata
@@ -116,6 +140,39 @@ class calModule():
         end = time.clock()
         print("生成统计数据及概率周期表完毕，用时：", end - start)
 
+    def createAllLuckyDataWithZone(self,scope,page,zone):
+        # print("开始生成预测的统计数据表和概率周期表.......")
+        # start = time.clock()
+        # lock = threading.Lock()
+        #
+        # def run(scope, page,zone):
+        #     lock.acquire()
+        #     try:
+        self.__modb.createLuckyTableWithZone(scope, page,zone)
+        #     finally:
+        #         lock.release()
+        #
+        # for p in range(page):
+        #     T = threading.Thread(target=run, args=(scope, p+1,zone))
+        #     T.start()
+        #     T.join()
+        # end = time.clock()
+        # print("生成统计数据及概率周期表完毕，用时：", end - start)
+
+        # 此方法为已有最新数据后的生成预测，只有再初始化数据或更新数据后可用
+    def createForecastDataWithZone(self,zone):
+            # 生成一个termdata
+            termData = self.__modb.getTermDatas(0)[0]
+            newTerm = term()
+            newTerm.termID = str(int(termData['termID']) + 1)
+            self.termForcast = newTerm.termID
+            newTerm.red = []
+            newTerm.blue = 0
+            # 将该数据插入到期数表
+            self.__modb.tterm.insert(newTerm.__dict__)
+            # 生成luckynum
+            self.createAllLuckyData(1, self.__page)
+
     def getLastOneTermData(self):
         return self.__modb.getTermDatas(0)[0]
 
@@ -125,18 +182,35 @@ class calModule():
     def getIntervalData(self,condition):
         return self.__modb.getIntervalDataByCondition(condition)
 
+    def getLuckyNumByDataID(self,dataID):
+        condition = {'dataID':dataID}
+        print(condition)
+        return self.__modb.tlucky.find({'dataID':dataID})[0]
+
+    def getLuckyNumByCondition(self,condition):
+        return self.__modb.tlucky.find(condition)
+
+    def saveResultToFile(self,fileName,termID,numSize):
+        with open(fileName, 'w') as f:
+            for data in test.getLuckyNumByCondition({'termID': termID}):
+                if data['numSize'] == numSize:
+                    lm = list(map(str, set(data['num'])))
+                    f.write("10/" + "~".join(lm) + "/N/0~1~2~3~4/N\r\n")
+
+
 if __name__ == '__main__':
     test = calModule()
-    test.initData(scope=200)
-    test.createForecastData()
-    test.termForcast = test.getLastOneTermID()
-    condition = {'termID': test.termForcast}
-    for data in test.getIntervalData(condition):
-        moduleID = data['moduleID']
-        groupID = data['groupID']
-        numSize = data['numSize']
-        #从概算周期表中找出moduleID和groupID相同的项
-        co = {'groupID':groupID,'moduleID':moduleID,'numSize':numSize,
-              'termID':{'$ne':test.termForcast}}
-        for sdata in test.getIntervalData(co):
-            print(sdata)
+    # test.initData(scope=200)
+    # test.createForecastData()
+    # test.termForcast = test.getLastOneTermID()
+    # condition = {'termID': test.termForcast}
+    # for data in test.getIntervalData(condition):
+    #     moduleID = data['moduleID']
+    #     groupID = data['groupID']
+    #     numSize = data['numSize']
+    #     #从概算周期表中找出moduleID和groupID相同的项
+    #     co = {'groupID':groupID,'moduleID':moduleID,'numSize':numSize,
+    #           'termID':{'$ne':test.termForcast}}
+    #     for sdata in test.getIntervalData(co):
+    #         print(sdata)
+    test.initDataWithZone()
